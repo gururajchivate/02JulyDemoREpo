9cb2f55fb1c42c4d20893f72b78ba788abe6bf28
gururajchivate <gchivate03@gmail.com>
2023-01-05 19:23:40 +0530

    Brach
    hi team i have created project

---
+++ DalalStreetProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gururaj
+ *
+ */
+module DalalStreetProject {
+}
---
+++ DalalStreetProject/src/myPackage/MyClass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class MyClass {
+
+	public static void main (String args[]) {
+		
+		System.out.println("Added Depedancies");
+		System.out.println("Added Depedancies");
+		System.out.println("Added Depedancies");
+		System.out.println("Added Depedancies");
+		
+	}
+}
